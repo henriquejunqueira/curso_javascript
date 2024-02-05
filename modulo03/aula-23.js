b421d09b7cc1a50b8a5065f3cdaec6b7a3cbdbc3
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-05 18:47:12 -0300

    aula-23

---
+++ modulo03/aula-23.js
@@ -0,0 +1,12 @@
+// função recursiva é chamada por ela mesma
+const fatorial = (valor) => {
+
+    if(valor === 0 || valor === 1){
+        return 1;
+    }
+
+    return valor * fatorial(valor - 1);
+}
+
+//console.log(fatorial(21));
+console.log(fatorial(100));
