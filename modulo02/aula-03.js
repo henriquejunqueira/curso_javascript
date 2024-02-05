28b15c1492651270de1f4c7e36b0eeaa6f92602b
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-04 23:00:07 -0300

    aula-03

---
+++ modulo02/aula-03.js
@@ -0,0 +1,24 @@
+const idade = 18;
+const nome = 'Henrique';
+//let podeEntrar = false;
+
+/*if(idade >= 18 || name === 'Henrique'){
+    //podeEntrar = true;
+    console.log('ENTROU');
+}else{
+    console.log('NAO ENTRA!!!');
+}*/
+
+/*if(idade >= 18 && nome === 'Henrique'){
+    //podeEntrar = true;
+    console.log('ENTROU');
+}else{
+    console.log('NAO ENTRA!!!');
+}*/
+
+if(idade == '18'){
+    //podeEntrar = true;
+    console.log('ENTROU');
+}else{
+    console.log('NAO ENTRA!!!');
+}
