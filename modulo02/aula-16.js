f63af9a3b135608e1568343f5cbcdd874d0c4411
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-05 12:25:50 -0300

    aula-16

---
+++ modulo02/aula-16.js
@@ -0,0 +1,12 @@
+const numero = 4324;
+const stringA = 'dsjfkajdfjkaj';
+const verdadeiroOuFalso = true; // false
+
+const arrayTest = [432, 6436, 643];
+
+stringA[4] = 'HENRIQUE';
+
+arrayTest.push(473985);
+
+console.log(arrayTest);
+console.log(stringA);
