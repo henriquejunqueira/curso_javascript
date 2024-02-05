3fa492a73f0dfae56499cec536e0c4dc7f75aa21
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-05 12:57:39 -0300

    aula-18

---
+++ modulo02/aula-18.js
@@ -0,0 +1,20 @@
+
+function calcular(valor){
+    if(valor === 0){
+        throw new Error('Não pode ser 0');
+    }
+
+    return valor / 2;
+}
+
+try{
+    //const resultado = calcular(654);
+    const resultado = calcular(0);
+    console.log('resultado: ', resultado);
+}catch(error){
+    console.log(error.message);
+}finally{
+    console.log('Chegou no finally');
+}
+
+console.log('continua');
