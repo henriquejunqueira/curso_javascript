7ecf494783969f0beaaeb29602152ef7e35e6772
henriquejunqueira <henriquejunqueira15@gmail.com>
2024-02-04 23:29:16 -0300

    aula 07

---
+++ modulo02/aula-07.js
@@ -0,0 +1,13 @@
+const idadeJulio = 18;
+const idadeJoao = 14;
+const idadeJose = 53;
+
+const idades = [18, 'eu', 563, 18, 194, 'joao', 187, 141, 523, 138, 154, 9953];
+
+for(let i = 0; i < idades.length; i++){
+    console.log('resultado', idades[i]);
+}
+
+//console.log('resultado', idades[0]);
+//console.log('tamanho array', idades.length);
+
